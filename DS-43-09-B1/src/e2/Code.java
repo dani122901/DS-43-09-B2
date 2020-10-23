a9cd66525aa8ca91e62cadfbf93dc7dec00363bc
dani122901 <73178901+dani122901@users.noreply.github.com>
2020-10-23 19:51:48 +0200

    Update Code.java

--- DS-43-09-B1/src/e2/Code.java
+++ DS-43-09-B1/src/e2/Code.java
@@ -5,47 +5,51 @@ public class Code {
     public static boolean isKeypadValid ( char [][] keypad ) {
 
 
-        boolean rows = false;
+        boolean columns = false; //Variable que nos indica como recorrer el teclado
 
-        if (keypad != null) {
+        if (keypad != null) { //Si el teclado no es nulo
 
             if ((keypad.length > 1) && (keypad[1] != null) && (keypad[1][0] == '2'))
-                rows = true;
 
-            int aux = 49;
+                columns = true;
 
-            if (rows) {
+            //Si el teclado tiene más de una fila y el primer elemento de la siguiente es un 2 significa que tenemos
+            //que recorrerlo en columnas, columns = true
 
-                for (int i = 0; i < keypad[0].length; i++) {
+            int aux = 49; //Variable para llevar cuenta del ascii en el que estamos (empezando en el 1)
 
-                    for (char[] position : keypad) {
+            if (columns) { //En columnas
 
-                        if (aux <= 57) {
+                for (int i = 0; i < keypad[0].length; i++) { //Bucle recorrer columnas
 
-                            if (position[i] != (char) aux)
+                    for (char[] position : keypad) { //Bucle recorrer filas
 
-                                return false;
+                        if (aux <= 57) { // Mientras no pasemos del número 9
 
-                            else aux++;
+                            if (position[i] != (char) aux) //Si el siguiente número del teclado no sigue el orden
+
+                                return false; //No es un teclado válido y devolvemos false
+
+                            else aux++; // Si es correcto seguimos con el siguiente
 
                         }
 
-                        if ((aux == 58) && (position[i] == '0'))
+                        if ((aux == 58) && (position[i] == '0')) //Cuando pasemos del 9 vendría el 0 en el teclado y vemos si es
 
-                            aux = 65;
+                            aux = 65; //Si es el 0 pasamos a las letras, empezando en la A (ascii 65)
 
-                        else if ((aux >= 65) && (aux <= 90)) {
+                        else if ((aux >= 65) && (aux <= 90)) { //Mientras sigamos entre la A y la Z
 
-                            if (position[i] != (char) aux)
+                            if (position[i] != (char) aux) //Si no siguen el orden
 
-                                return false;
+                                return false; //Devolvemos falso al no ser válido el teclado
 
-                            else aux++;
+                            else aux++; //Si no seguimos con la siguiente
 
                         }
                     }
                 }
-            } else {
+            } else { //Lo mismo pero recorriéndolo por filas
 
                 for (char[] position : keypad) {
 
@@ -84,17 +88,19 @@ public class Code {
 
     public static boolean areMovementsValid ( String [] movements ) {
 
-        if (movements != null) {
+        if (movements != null) { //Si el string de movimientos no es nulo lo recorremos
 
             for (String movement : movements) {
 
-                if (movement != null) {
+                if (movement != null) { //No puede haber movimientos nulos
 
                     for (int j = 0; j < movement.length(); j++) {
 
                         if (movement.charAt(j) != 'R' && movement.charAt(j) != 'L' && movement.charAt(j) != 'U' && movement.charAt(j) != 'D') {
 
                             return false;
+
+                            //Si los desplazamientos dentro de cada movimiento no son válidos (L, R, U, D) devolvemos falso
 
                         }
                     }
@@ -112,16 +118,18 @@ public class Code {
 
     public static String obtainCode ( char [][] keypad , String [] movements ) {
 
-        String finalCode = "";
+        String finalCode = ""; //El string donde almacenamos el código
 
         int row = 0;
         int column = 0;
 
-        for (String movement : movements) {
+        //Variables para saber el dígito del código
 
-            if (movement != null) {
+        for (String movement : movements) { //Vamos recorriendo los movimientos
 
-                for (int n = 0; n < movement.length(); n++) {
+            if (movement != null) { //Si no es nulo
+
+                for (int n = 0; n < movement.length(); n++) { //Según nos diga el movimiento nos movemos en la fila o la columna
 
                     if (column < (keypad[0].length - 1) && movement.charAt(n) == 'R') {
 
@@ -143,15 +151,15 @@ public class Code {
 
                 }
 
-                if (row >= 0 && row < keypad.length && column >= 0 && column < keypad[0].length)
+                if (row >= 0 && row < keypad.length && column >= 0 && column < keypad[0].length) //Si nos indica una posicion válida del teclado
 
-                    finalCode = finalCode + keypad[row][column];
+                    finalCode = finalCode + keypad[row][column]; //Agregamos al string el dígito obtenido
 
                 else throw new IllegalArgumentException("Invalid position");
             }
         }
 
-        return finalCode;
+        return finalCode; //Devolvemos el código final
     }
 
 }
